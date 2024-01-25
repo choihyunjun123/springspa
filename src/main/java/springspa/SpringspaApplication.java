777bdf8410acbe85b704f0d06503b5f64f8763a9
HYUNJUN\sajun <sajun28@naver.com>
2024-01-25 10:58:52 +0900

    no message

---
+++ src/main/java/springspa/SpringspaApplication.java
@@ -0,0 +1,11 @@
+package springspa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringspaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringspaApplication.class, args);
+    }
+}
